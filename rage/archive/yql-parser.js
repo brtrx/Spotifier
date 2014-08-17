54628acfdb107791f1c6494206f6b4c7d3086f62
brtrx <justintauber@proferos-mbp-5.home.gateway>
2014-08-17 10:57:59 +1000

    Initialise repo
    Two separate parts: spotify authorisation flow (example and first
    adaptation) and Rage playlist screen scraper

---
+++ rage/archive/yql-parser.js
@@ -0,0 +1,43 @@
+// ABC Rage Spotifier - YQL parser
+
+/* Structure
+
+Get each playlist as an array [list-datetime, list-url]
+- div#listContainer
+-- each child div is a month, where id = month+"_content", i.e. split as "_" and discard remainder
+
+*/
+
+	// dependencies
+    var http = require('http')
+    var YQL = require('yql')
+    var listNum = process.argv[2]
+
+	// this is a very specific application at the moment
+    var rageURL = "http://www.abc.net.au/rage/playlists/",
+    	rageCSSSelector = "#listContainer div ul li"
+
+
+	// Uses YQL to get all the playlist urls within a particular css selector
+	function getPlayListURLs (siteUrl, cssSelector, callback) {		
+		
+		var query = 'select * from data.html.cssselect where url="'+siteUrl+'" and css="'+cssSelector+'"'
+		new YQL.exec(query, function(response) {
+			callback(response.query.results.results.li)
+		});
+	}
+
+
+	getPlayListURLs (rageURL, rageCSSSelector, function (res) {
+		var urlArray = []
+		res.forEach( function(element) {
+			if(typeof element.div[0].p.span === 'undefined') {
+				urlArray.push({'date':element.div[0].p, 'url':element.a.href})
+			} else {
+				urlArray.push({'date':element.div[0].p.content, 'url':element.a.href})
+			}
+		});
+		// Showing how to reference date and url separately
+		console.log("Number " + listNum + " of " + urlArray.length + "\n" + urlArray[4].date + " - " + urlArray[4].url)
+		
+	})
---
+++ rage/song-parser.js
@@ -0,0 +1,73 @@
+// ABC Rage Spotifier - YQL parser
+
+/*
+
+Get each song from a particular playlist as an array [artist, [featured-artist,] song-title] 
+(- find div.playlist)
+- for each p.list
+--- split by <br> to separate songs
+----- strong.innerhtml = artist -> split by "featuring" to extract featured-artist
+----- em.innerhtml = song-title
+----- em.sibling = song-publisher (NOT NEEDED)
+
+Needs a:
+- song-extractor
+- song-parser
+
+*/
+
+	// This program takes a playlist url, and produces an ordered song array
+
+	// dependencies
+    var http = require('http')
+    var YQL = require('yql')
+	var pageURL = process.argv[2],
+		songsDIV = "p.list"
+
+	// Uses YQL to get all the playlist urls within a particular css selector
+	function getPlaylist (siteURL, cssSelector, callback) {		
+
+		var query = 'select * from data.html.cssselect where url="'+siteURL+'" and css="'+cssSelector+'"'
+		new YQL.exec(query, function(response) {
+			callback(response.query.results.results.p)
+		});
+	}
+
+	String.prototype.toTitleCase = function ()
+	{
+		return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	}
+
+	
+		getPlaylist (pageURL, songsDIV, function(res) {
+			//console.log(res)
+			//console.log(res[res.length-1].strong.length)
+
+			var songArray = []
+			var count = 0
+		
+			for (var i=0; i<res.length; i++) {
+				var slotSongs = res[i]
+			
+				// DEBUG
+				if (i==4) console.log(slotSongs)
+			
+				// for each song
+				for (var j=0; j<slotSongs.strong.length; j++) {
+					var artists = slotSongs.strong[j].split("featuring")
+					var title = slotSongs.em[j]
+				
+					songArray.push(
+						{
+							'id':count,
+							'artist':artists[0].toTitleCase(),  // 'feature-artist':artists[1],
+							'title':title, 
+						 })
+					count++
+				}
+			}	
+			songArray.forEach( function(data) {
+				console.log(data)
+			})
+		})
+	
